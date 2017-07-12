9e7550d986745c45a897459b00aaeae5eae629f8
ijazm <ijazmohamed.ijaz@gmail.com>
2017-07-12 11:07:29 +0530

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -1,61 +1,24 @@
 pragma solidity ^0.4.0;
 
-/**
- * Project data contract
- */
-contract Project {
+
+contract Project  {
+  // NOTE: members must be public to be indexed for search
   enum ErrorCodes { NULL, SUCCESS, ERROR, NOT_FOUND, EXISTS, RECURSIVE, INSUFFICIENT_BALANCE }
   ErrorCodes public errorCodes;
   enum ProjectState { NULL, OPEN, PRODUCTION, INTRANSIT, RECEIVED }
-  //ProjectState public state;
-  // NOTE: members must be public to be indexed for search
+  ProjectState public state;
+  
   string public name;
   string public buyer;
   string public description;
-  string public spec;
-  uint public price; // in cents
+  uint public price;
 
-  uint public created; // date
-  uint public targetDelivery; // date
-  uint public delivered; // date
-
-  string public addressStreet;
-  string public addressCity;
-  string public addressState;
-  string public addressZip;
-
-  ProjectState public state;
-
-  function Project(
-    string _name,
-    string _buyer,
-    string _description,
-    string _spec,
-    uint _price,
-    uint _created,
-    uint _targetDelivery
-  ) {
+  function Project( string _name, string _buyer, string _description, uint _price,) {
     name = _name;
     buyer = _buyer;
     description = _description;
-    spec = _spec;
     price = _price;
-    created = _created;
-    targetDelivery = _targetDelivery;
-
     state = ProjectState.OPEN;
-  }
-
-  function setShippingAddress(
-    string _addressStreet,
-    string _addressCity,
-    string _addressState,
-    string _addressZip
-  ) {
-    addressStreet = _addressStreet;
-    addressCity = _addressCity;
-    addressState = _addressState;
-    addressZip = _addressZip;
   }
 
   function getState() returns (ProjectState) {
@@ -66,3 +29,4 @@ contract Project {
     state = _state;
   }
 }
+
