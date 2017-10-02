db3575c707f6b541d9f236b527a3319b2ad77fdc
ijazm <ijazmohamed.ijaz@gmail.com>
2017-10-02 21:10:56 +0530

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -1,8 +1,8 @@
 interface IERC20 {
     function totalSupply() constant returns (uint256 totalSupply);
     function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 _ value) returns (bool Success);
-    function transferFrom(address _from, address _to, uint256 _ value) returns (bool Success);
+    function transfer(address _to, uint256 _value) returns (bool Success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool Success);
     function approve(address _spender, uint256 _ value) returns (bool success);
     function allowance(address _owner, adddress _spender) constant returns (uint256 remaining);
     event Transfer(address indexed_form, address indexed_to, uint256 _value);
